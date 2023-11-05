3390edcf69567c3eca662c2c0183bd5a41eb63c7
Alex Marín <kysgeneral.nrgua@slmail.me>
2023-11-05 19:03:23 +0100

    UI Improvments

---
+++ delphinus-ui/src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+   <v-footer>
+      <v-row justify="center" no-gutters>
+         <v-btn variant="text" class="mx-2" icon="mdi-mastodon" href="https://mastodon.social/@mariinkys"
+            target="_blank"></v-btn>
+         <v-btn variant="text" class="mx-2" icon="mdi-github" href="https://github.com/mariinkys/delphinus_flashcards"
+            target="_blank"></v-btn>
+         <v-btn variant="text" class="mx-2" icon="mdi-coffee" href="https://www.buymeacoffee.com/mariinkys"
+            target="_blank"></v-btn>
+         <v-col class="text-center mt-4" cols="12">
+            {{ new Date().getFullYear() }} — <strong>Alex Marín</strong> — FLOSS, now and always!
+         </v-col>
+      </v-row>
+   </v-footer>
+</template>
--- delphinus-ui/src/components/custom-title.vue
+++ delphinus-ui/src/components/custom-title.vue
@@ -1,5 +1,5 @@
 <template>
-   <h1 class="text-center mb-3 text-h3">{{ props.headerText }}</h1>
+   <h1 class="text-center mb-3 text-h3" style="font-weight: bold;">{{ props.headerText }}</h1>
 </template>
 
 <script setup lang="ts">
