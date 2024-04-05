032fcebd99dfd530fd2a0f17b6179c0ed8bda326
mariinkys <kysgeneral.nrgua@slmail.me>
2024-04-05 19:23:23 +0200

    always default to light theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   daisyui: {
     logs: false, // https://github.com/leptos-rs/cargo-leptos/issues/136
     themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "dark", // name of one of the included themes for dark mode
+    darkTheme: "light", // theme to be the default dark mode theme.
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
